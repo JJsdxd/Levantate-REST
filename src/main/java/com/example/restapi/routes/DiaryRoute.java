04470086993b3a426a29024d491d15ddc867d77f
Snape25 <nandagopalvs12@gmail.com>
2021-09-04 11:21:32 +0530

    Route added

--- src/main/java/com/example/restapi/routes/DiaryRoute.java
+++ src/main/java/com/example/restapi/routes/DiaryRoute.java
@@ -2,17 +2,19 @@ package com.example.restapi.routes;
 
 
 import com.example.restapi.pojos.DiaryRequest;
+import com.example.restapi.pojos.DiaryResponse;
+import com.example.restapi.pojos.SimplePost;
 import com.example.restapi.pojos.StoryRequest;
 import com.example.restapi.services.DiaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class DiaryRoute {
@@ -29,6 +31,18 @@ public class DiaryRoute {
 
         return new ResponseEntity<>("Created Sucesfully", HttpStatus.OK);
 
+
+    }
+
+    @CrossOrigin
+    @GetMapping("/diary")
+    public Map<String,Object> GetAllDiary(HttpServletRequest request){
+        int token_user_id = (Integer) request.getAttribute("userId");
+        List<DiaryResponse> diary_response =  diaryService.GetAllDiary(token_user_id);
+        Map<String, Object> map = new HashMap<>();
+        map.put("ListOfDiaryEntries", diary_response);
+
+        return map;
 
     }
 }
