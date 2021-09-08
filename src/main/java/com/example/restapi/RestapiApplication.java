e140568cc29b32291d52bb5e7e490950800a4fc9
Nandagopal <nandu@pop-os.localdomain>
2021-09-08 19:22:17 +0530

    t9

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -33,7 +33,15 @@ public class RestapiApplication {
 
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
 
 
