4439f2d94fea3686abefdb3c3c3e0c6b22268ec6
Nandagopal <nandu@pop-os.localdomain>
2021-09-08 20:26:27 +0530

    t9

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -4,7 +4,11 @@ import com.example.restapi.filters.Authfilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.RegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -16,6 +20,19 @@ public class RestapiApplication {
         SpringApplication.run(RestapiApplication.class, args);
     }
 
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsfilter(){
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("https://youthful-heyrovsky-a6b78e.netlify.app");
+        corsConfiguration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**",corsConfiguration);
+    registrationBean.setFilter(new CorsFilter(source));
+    registrationBean.setOrder(0);
+    return registrationBean;
+    }
 
     @Bean
     public FilterRegistrationBean<Authfilter> filterRegistrationBean(){
